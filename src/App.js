8e1d8df133c6421d2e56e7cdd83553e559c03f3b
ihebkhalloufi <ihebkhaloufi48@gmail.com>
2021-11-29 09:53:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+
+function App() {
+  return (
+<div>
+    <div className="border">
+<h1 className="title">willcom in my project</h1>
+<div className="Nav">
+</div>
+</div>
+<div>
+  <img className="image" src="index1.jpg"></img>
+</div>
+<div>
+<img className="image" src="index2.jpg"></img>
+<div></div>
+
+<iframe width="853" height="480" src="https://www.youtube.com/embed/_eTtCUZGDeY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+
+
+</div>
+</div>
+    
+  );
+}
+
+export default App;
